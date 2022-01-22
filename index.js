695a17dca1409887bad4da1daedf925810f213a6
DCT <mirachan2332@gmail.com>
2022-01-22 15:07:41 -0600

    created questions for prompting

--- index.js
+++ index.js
@@ -1,4 +1,66 @@
-// TODO: Include packages needed for this application
+//Packages Needed to Run:
+//const inquirer required to run the inquirer package
+const inquirer = require('inquirer');
+
+//const fs required to run node.js fs module
+const fs = require('fs');
+
+//const path module required for file and directory pathing
+const path = require('path');
+
+//const generateMarkdown required for connecting index.js to generateMarkdown.js
+const generateMarkdown = require ('./utils/generateMarkdown');
+
+//question array for obtaining user input
+const questions = [
+    {
+        name: 'title',
+        type: 'input',
+        message: 'Project name?'
+    },
+    {
+        name: 'description',
+        type: 'input',
+        message: 'Describe your project.'
+    },
+    {
+        name: 'installation',
+        type: 'input',
+        message: 'Installation instructions.'
+    },
+    {
+        name: 'usage',
+        type: 'input',
+        message: 'How is your project used?'
+    },
+    {
+        name: 'license',
+        type: 'list',
+        message: 'Choose license(s).'
+        choices: ['', 'Apache', 'GNU', 'MIT', 'BSD', 'Boost', 'Creative', 'Eclipse', 'Mozilla', 'The Unlicense']
+    },
+    {
+        name: 'contribution',
+        type: 'input',
+        message: 'How can others contribute?'
+    },
+    {
+        name: 'tests',
+        type: 'input',
+        message: 'Any tests?'
+    },
+    {
+        name: 'github',
+        input: 'input',
+        message: 'Enter your github username.'
+    },
+    {
+        name: 'email',
+        type: 'input',
+        message: 'Enter your email address.'
+    },
+];
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
